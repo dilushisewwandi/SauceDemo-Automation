932b48340bfe5032c5db4c3562780f9a34100908
Dilushika <dilushisewwandi89@gmail.com>
2025-09-07 17:54:01 +0530

    Test(LoginTest): update error-user, performance-glitch-user tests and add assertions

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -7,14 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 
 public class LoginTest {
@@ -47,7 +45,6 @@ public class LoginTest {
         softAssert.assertEquals(currentURL, expectedURL, "User not redirected to inventory page after login");
         softAssert.assertAll();
 
-        System.out.println("Login Successful!");
     }
 
     @Test(priority = 1, dataProvider = "invalidPasswordData", dataProviderClass = LoginDataProvider.class)
@@ -64,7 +61,6 @@ public class LoginTest {
         softAssert.assertTrue(currentURL.equals(expectedURL), "User should remain on login page");
         softAssert.assertAll();
 
-        System.out.println("Invalid password test is passed");
     }
 
     @Test(priority = 2, dataProvider = "emptyFieldsData", dataProviderClass = LoginDataProvider.class)
@@ -80,7 +76,6 @@ public class LoginTest {
         softAssert.assertTrue(currentURL.equals(expectedURL), "User should remain on login page");
         softAssert.assertAll();
 
-        System.out.println("Empty fields test is passed");
     }
 
     @Test(priority = 3, dataProvider = "lockedUserData", dataProviderClass = LoginDataProvider.class)
@@ -96,43 +91,32 @@ public class LoginTest {
         softAssert.assertTrue(currentURL.equals(expectedURL), "User should remain on login page");
         softAssert.assertAll();
 
-        System.out.println("Locked out user test is passed");
     }
 
     @Test(priority = 4, dataProvider = "problemUserData", dataProviderClass = LoginDataProvider.class)
-    public void problemUserLogin(String username, String password) throws InterruptedException {
-        login("standard_user", "secret_sauce");
+    public void problemUserTest(String username, String password){
+        login("standard_user","secret_sauce");
 
-        List<WebElement> standardImages = driver.findElements(By.xpath("//div[@class='inventory_item_img']/a/img"));
-        List<String> expectedImages = new ArrayList<>();
-        for (WebElement images: standardImages
-             ) {
-            expectedImages.add(images.getAttribute("src"));
-        }
-        Thread.sleep(3000);
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
 
-        driver.get("https://www.saucedemo.com/");
-        login(username, password);
+        WebElement standardImage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='inventory_item_img']/a/img")));
+        String imgSrc1 = standardImage.getAttribute("src");
+        System.out.println("Standard Image src: " + imgSrc1);
 
+        driver.navigate().back();
 
-        String currentURL = driver.getCurrentUrl();
-        String expectedURL = "https://www.saucedemo.com/inventory.html";
-        softAssert.assertEquals(currentURL, expectedURL, "User not redirected to inventory page after login");
+        wait.until(ExpectedConditions.urlContains("https://www.saucedemo.com/"));
 
-        List<WebElement> productImages = driver.findElements(By.xpath("//div[@class='inventory_item_img']/a/img"));
-        List<String> actualImages = new ArrayList<>();
+        login(username,password);
 
-        for (WebElement images : productImages
-        ) {
-            actualImages.add(images.getAttribute("src"));
-        }
+        wait.until(ExpectedConditions.urlContains("https://www.saucedemo.com/inventory.html"));
+        softAssert.assertTrue(driver.getCurrentUrl().contains("inventory.html"),"URL is incorrect after login");
 
-        if (actualImages.equals(expectedImages)) {
-            System.out.println("Problem user test failed");
-        } else {
-            System.out.println("Problem user test passed");
-        }
+        WebElement currentImage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='inventory_item_img']/a/img")));
+        String imgSrc2 = currentImage.getAttribute("src");
+        System.out.println("Current Image src: " + imgSrc2);
 
+        softAssert.assertNotEquals(imgSrc1,imgSrc2,"This image can not be displayed after login problem user. Test failed");
         softAssert.assertAll();
     }
 
@@ -144,24 +128,27 @@ public class LoginTest {
         String expectedURL = "https://www.saucedemo.com/inventory.html";
         softAssert.assertEquals(currentURL, expectedURL, "User not redirected to inventory page after login");
 
-        Thread.sleep(3000);
-        WebElement addtoCartButtton = driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt"));
-        addtoCartButtton.click();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+        WebElement addToCartButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-sauce-labs-bolt-t-shirt")));
+        addToCartButton.click();
 
-        List<WebElement> cartBadgeCount = driver.findElements(By.xpath("//span[@class='shopping_cart_badge']"));
+        List<WebElement> cartBadge = driver.findElements(By.xpath("//span[@class='shopping_cart_badge']"));
 
-        if (cartBadgeCount.size()>0){
-            System.out.println("Add to cart buttons work properly");
+        if (cartBadge.isEmpty()){
+            softAssert.assertTrue(true,"Cart badge not increased as expected for error user");
+        }else {
+            String cartBadgeCount = cartBadge.get(0).getText();
+            int count  = Integer.parseInt(cartBadgeCount);
+            softAssert.assertEquals(count,"Cart badge increased unexpectedly for error user");
         }
-        else {
-            System.out.println("Add to cart buttons not work properly");
-        }
+        softAssert.assertAll();
     }
 
     @Test(priority = 6, dataProvider = "performanceGlitchUserData", dataProviderClass = LoginDataProvider.class)
     public void performanceGlitchUserLogin(String username, String password){
 
         long startTime = System.currentTimeMillis();
+        System.out.println(startTime);
 
         login(username,password);
 
@@ -169,68 +156,15 @@ public class LoginTest {
         wait.until(ExpectedConditions.urlContains("inventory.html"));
 
         long endTime = System.currentTimeMillis();
+        System.out.println(endTime);
         long responseTime = (endTime - startTime)/1000;
+        System.out.println(responseTime);
 
-        if (responseTime<3){
-            Assert.fail("Response time is " + responseTime + " Login was too fast. Test Failed");
-        } else if (responseTime>=3 && responseTime<=6) {
-            System.out.println("Response time is " + responseTime + " Login delayed as expected. Test Passed");
-        }else {
-            Assert.fail("Response time is " + responseTime + " Login was too slow. Test Failed");
-        }
+        softAssert.assertTrue(responseTime>=4 && responseTime<=7,"Login was not delayed as expected. Response time is: " + responseTime);
 
         String currentURL = driver.getCurrentUrl();
         String expectedURL = "https://www.saucedemo.com/inventory.html";
         softAssert.assertEquals(currentURL, expectedURL, "User not redirected to inventory page after login");
-        softAssert.assertAll();
-
-    }
-
-    @Test(priority = 7, dataProvider = "visualUserData", dataProviderClass = LoginDataProvider.class)
-    public void visualUserLogin(String username, String password){
-        login(username, password);
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.urlContains("inventory.html"));
-
-        String currentURL = driver.getCurrentUrl();
-        String expectedURL = "https://www.saucedemo.com/inventory.html";
-        softAssert.assertEquals(currentURL, expectedURL, "User not redirected to inventory page after login");
-//
-//        WebElement pricebar = driver.findElement(By.xpath("//div[@class='pricebar']"));
-//        pricebar.getSize();
-//        WebElement itemPrice = driver.findElement(By.xpath("//div[@class='inventory_item_price']"));
-//        itemPrice.getSize();
-//        WebElement addToCartButton = driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-onesie']"));
-//        addToCartButton.getSize();
-//
-//        long spaceBetween = pricebar.getSize() - ((itemPrice.getSize())+(addToCartButton.getSize()));
-//        System.out.println("Space between is " + spaceBetween);
-//
-//        if (spaceBetween>3){
-//            System.out.println("Elements are overlapped. Visual user login test passed");
-//        }else {
-//            Assert.fail();
-//        }
-//
-//        softAssert.assertAll();
-        WebElement itemPrice = driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[6]/div[2]/div[2]/div[1]"));
-        WebElement addToCartButton = driver.findElement(By.xpath("//button[@id='add-to-cart-test.allthethings()-t-shirt-(red)']"));
-
-        int itemPriceX = itemPrice.getLocation().getX();
-        int addToCartButtonX = addToCartButton.getLocation().getX();
-
-        int spaceBetween = addToCartButtonX-itemPriceX;
-
-        System.out.println("Space between Item Price and Add to Cart Button is: " + spaceBetween + "px");
-
-        // Validate layout consistency
-        if (spaceBetween > 512) { // threshold, adjust if needed
-            Assert.fail("FAIL: Space is too large (" + spaceBetween + "px). Button may overlap.");
-        } else {
-            System.out.println("PASS: Layout is consistent. No overlap detected.");
-        }
-
         softAssert.assertAll();
     }
 
